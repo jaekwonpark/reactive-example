f5e1aa250fa22d8ff8683c9cd71b0b498ca64a19
Jae Park <jae.park@nutanix.com>
2020-06-24 16:12:24 -0700

    Initial version

---
+++ src/test/java/com/myreactive/reactiveexample/ReactiveExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myreactive.reactiveexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
